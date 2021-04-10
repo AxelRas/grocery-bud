8d4165843f18ea3182bca8b6509c0e68e4859d7b
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-04-10 10:28:56 -0300

    Working but only with items with no spaces

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import { Component } from "react";
 import Items from './components/Items';
 import $ from 'jquery';
 
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      currentlyEditing: {}
     }
 
     this.addItem = this.addItem.bind(this);
     this.removeItem = this.removeItem.bind(this);
     this.editItem = this.editItem.bind(this);
+    this.confirmEdit = this.confirmEdit.bind(this);
+  }
+
+  componentDidUpdate() {
+    // console.log(this.state.test);
   }
 
 
@@ -26,40 +33,72 @@ class App extends Component {
     }
 
     this.setState({
-      items: [...this.state.items, item]
+      items: [...this.state.items, item],
+      currentlyEditing: { ...this.state.currentlyEditing, [item]: false }
     });
     document.getElementById('input-box').value = '';
   }
 
   removeItem(itemId) {
     this.setState({
-      items: this.state.items.filter(item => item !== itemId)
+      items: this.state.items.filter(item => item !== itemId),
+      currentlyEditing: { ...this.state.currentlyEditing, itemId: undefined }
     });
+  }
+
+  confirmEdit(itemId) {
+    let newItem = document.getElementById(`${itemId}-edit-input`).value;
+    if (this.state.items.includes(newItem)) {
+      alert('Item already on list');
+      return;
+    }
+
+    $(`#${itemId}-edit-input`).remove();
+    $(`<p>${newItem}</p>`).insertBefore(`#${itemId} .item-actions`);
+
+    let newItemsList = [...this.state.items].filter(item => item !== itemId);
+    newItemsList.push(newItem);
+
+    let newCurrentlyEditing = { ...this.state.currentlyEditing }
+    delete newCurrentlyEditing[itemId];
+    newCurrentlyEditing[newItem] = false
+    this.setState({
+      items: newItemsList,
+      currentlyEditing: newCurrentlyEditing
+    })
+
   }
 
   editItem(itemId) {
     // FIX ID, NO SPACES ALLOWED
-    $(`#${itemId} p`).remove();
+    if (this.state.currentlyEditing[itemId] === false) {
 
-    $(`<input class="edit-input" id="${itemId}-edit-input" type="text" value="${itemId}"></input>`).insertBefore(`#${itemId} .item-actions`);
+      this.setState({
+        currentlyEditing: { ...this.state.currentlyEditing, [itemId]: true }
+      });
 
-    $(`#${itemId}-edit-input`).css({
-      "font-family": "'Josefin Sans'",
-      "font-weight": "300",
-      "font-size": "1.2em",
-      "background-color": "rgba(255, 255, 255, 0.2)",
-      "border": "none",
-      "margin": "0",
-      "padding": "0",
-      "width": "200px"
-    });
+      $(`#${itemId} p`).remove();
 
-    document.getElementById(`${itemId}-edit-input`).focus();
+      $(`<input class="edit-input" id="${itemId}-edit-input" type="text" value="${itemId}"></input>`).insertBefore(`#${itemId} .item-actions`);
 
-    // FIND A WAY TO REPLACE BUTTON FUNCTION
+      $(`#${itemId}-edit-input`).css({
+        "font-family": "'Josefin Sans'",
+        "font-weight": "300",
+        "font-size": "1.2em",
+        "background-color": "rgba(255, 255, 255, 0.2)",
+        "border": "none",
+        "margin": "0",
+        "padding": "0",
+        "width": "200px"
+      });
 
-    document.getElementById(`${itemId}-edit-btn`).removeAttribute('onclick');
-    $(`#${itemId}-edit-btn`).html('Confirm');
+      document.getElementById(`${itemId}-edit-input`).focus();
+
+      $(`#${itemId}-edit-btn`).html('Confirm');
+    } else {
+      this.confirmEdit(itemId);
+    }
+
 
   }
 
